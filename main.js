78657562e1f427f3eb8ba46d2394e3608ab8a76d
Austin Mangum <mangumaustin@gmail.com>
2023-08-01 18:46:27 -0500

    commit

--- main.js
+++ main.js
@@ -20,3 +20,23 @@ const students = [
 ];
 
 const expelledStudents = [];
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+const studentsCard = (Card) => {
+  let domString = "";
+  for (const students of Card) {
+    domString += `<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+  }
+
+}
