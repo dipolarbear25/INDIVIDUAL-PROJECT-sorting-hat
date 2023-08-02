7a9def2e69a96e58a47935752519389bc2667cc1
Austin Mangum <mangumaustin@gmail.com>
2023-08-01 20:24:14 -0500

    commit

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ const renderToDom = (divId, htmlToRender) => {
 };
 
 const studentsCard = () => {
-  for (let domString of students) {
+  for (let  of students) {
     domString += `<div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
