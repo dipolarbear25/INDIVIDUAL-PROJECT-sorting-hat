a916fd884f5039386b44564aa29021d47934d368
Austin Mangum <mangumaustin@gmail.com>
2023-08-01 20:20:27 -0500

    commit

--- main.js
+++ main.js
@@ -27,11 +27,11 @@ const renderToDom = (divId, htmlToRender) => {
 };
 
 const studentsCard = () => {
-  for (let student of students) {
-    domString = `<div class="card" style="width: 18rem;">
+  for (let domString of students) {
+    domString += `<div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${student.name}</h5>
+      <h5 class="card-title">${students.name}</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
