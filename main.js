f19e022a42a29f7f06d3c0d7c8d4d5307c82330c
Austin Mangum <mangumaustin@gmail.com>
2023-08-05 13:35:49 -0500

    commit

--- main.js
+++ main.js
@@ -23,30 +23,27 @@ const expelledStudents = [];
 
 
 
-const renderToDom = (divId, htmlToRender) => {
-  const selectedDiv = document.querySelector(divId);
+const renderToDom = ("wizard-card", htmlToRender) => {
+  const selectedDiv = document.querySelector("#wizard-card");
   selectedDiv.innerHTML = htmlToRender;
 };
 
-const studentsCard = () => {
-  for (let name of students) { 
-    domString += `<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">.</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`;
-  }
-  
-  renderToDom("wizard-card", domString);
-}
+let domString = ""
 
-
-
-
+for (const student of students) {
+      domString += `<div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${student.name}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>`;
+    };
 const startApp = () => {
   studentsCard();
 }
 startApp();
+
+
+//cards need to be rendered and a form to create the cards and be able to submit the form. by MONDAY.
