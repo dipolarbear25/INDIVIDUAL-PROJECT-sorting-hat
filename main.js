493c36fc01822e5f62d16fbb4f4b338ecdb11c5c
Austin Mangum <mangumaustin@gmail.com>
2023-08-05 14:28:41 -0500

    commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const students = [
+const students = [                     //object for examples of students in houses
   {
     id: 1,
     name: "Harry Potter",
@@ -30,8 +30,8 @@ for (const student of students) {
       domString += `<div class="card" style="width: 18rem;">
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${student.name}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">${student.house}</h5>
+        <p class="card-text">${student.name}</p>
         <a href="#" class="btn btn-primary">Expel</a>
       </div>
     </div>`;
@@ -39,4 +39,7 @@ for (const student of students) {
 
 targeting.innerHTML = domString;
 
+function doSomething(){
+
+}
 //cards need to be rendered (done) and a form to create the cards and be able to submit the form. by MONDAY.
