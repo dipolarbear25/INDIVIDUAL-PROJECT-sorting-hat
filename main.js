83e2b86c414b8de9d45a8735054867204540bdc5
Austin Mangum <mangumaustin@gmail.com>
2023-08-06 17:23:51 -0500

    commit

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const students = [                     //object for examples of students in hous
 
 const expelledStudents = [];
 
-const submit = document.getElementById("formId")
+const submit = document.getElementById("submit")
 
 const form = document.getElementById("formId")
 
