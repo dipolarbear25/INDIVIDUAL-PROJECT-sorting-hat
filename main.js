c5688fac2c982827558042dbb9a49527233f1ca9
Austin Mangum <mangumaustin@gmail.com>
2023-08-06 17:35:32 -0500

    commit

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const form = document.getElementById("formId")
 
 const input = document.getElementById("fnameId")
 
-console.log(fnameid);
+console.log("hello");
 
 //const targeting = document.querySelector("wizard-card");
 
