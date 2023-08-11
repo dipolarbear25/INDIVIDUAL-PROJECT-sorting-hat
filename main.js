5a40b0014facd8b134743e798bf1aa27170e7cf2
Austin Mangum <mangumaustin@gmail.com>
2023-08-11 12:24:07 -0500

    commit

--- main.js
+++ main.js
@@ -32,27 +32,36 @@ const expelledStudents = [];
 const cardsOnDom = () => {
   let cards = "";
 for (const student of students) {
-  cards += `<div class="card" id="stuCard" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-  <h5 class="card-title">${student.house}</h5>
-  <p class="card-text">${student.name}</p>
-  <a href="#" class="btn btn-primary" id="expelbtn">Expel</a>
+  cards += `<div class="card mb-3" style="max-width: 500px;">
+  <div class="row g-0">
+    <div class="col-md-4"style="max-width:150px;background-color:${student.house == "Ravenclaw" ? "#0b304a" : student.house == "Slytherin" ? "#25581f" : student.house == "Gryffindor" ? "#4d0506" : "#f3de0b"};">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${student.name}</h5>
+        <p class="card-text">${student.house}</p>
+        <p class="card-text"><small class="text-body-secondary"><button type="button" class="btn btn-outline-danger" id='expel--${student.studentId}'>Expel</button></small></p>
+      </div>
+    </div>
   </div>
-  </div>`;
+  </div>`
     }
   renderToDom("#wizard-card", cards) 
   };
 
-  function pickRandomProperty(obj) {
-    var result;
-    var count = 0;
-    for (var prop in obj)
-        if (Math.random() < 1/++count)
-           result = prop;
-    return result;
-}
-console.log(pickRandomProperty(students));
+const form = document.querySelector(`#formId`)
+
+const stuProp = Object.keys(students);
+
+const randomPropNum = Math.floor(Math.random() * stuProp.length)
+
+const randomPropName = stuProp[randomPropNum]
+
+const randomPropValue = students[randomPropName]
+
+const randomValue = Math.floor(Math.random() * randomPropValue.length)
+
+console.log(randomPropValue[randomValue]);
 
 const createStudent = (e) => {
   e.preventDefault();
